06dac9aba4a7767e32aac4c0039e4ea3f618c398
gyuri <jae04099@catholic.ac.kr>
2022-01-04 21:17:34 +0900

    :sparkles: add card component

--- src/components/MyPage/Card/MyCard.tsx
+++ src/components/MyPage/Card/MyCard.tsx
@@ -17,7 +17,16 @@ export const MyCard = () => {
             <a href="/@id/dfjsfd">
                 <h2>lorem ipsum</h2>
             </a>
-            <p>lorem ipsum lorem ipsum 혹시 그럼 이런 글도 혹시 쓰실 생각 있으실까요? 제 질문이지만 아마 다른 분들도 궁금해할 질문같아서요 ㅎㅎ 1. 프론트엔드 포지션이 생긴 이유, 앞으로 어떻게 될지 2. 프론트엔드 개발자의 업무 범위 3. 잘하는 프론트엔드 개발자란? 혹시 그럼 이런 글도 혹시 쓰실 생각 있으실까요? 제 질문이지만 아마 다른 분들도 궁금해할 질문같아서요 ㅎㅎ 1. 프론트엔드 포지션이 생긴 이유, 앞으로 어떻게 될지 2. 프론트엔드 개발자의 업무 범위 3. 잘하는 프론트엔드 개발자란?</p>
+            <p>
+                lorem ipsum lorem ipsum 혹시 그럼 이런 글도 혹시 쓰실 생각
+                있으실까요? 제 질문이지만 아마 다른 분들도 궁금해할 질문같아서요
+                ㅎㅎ 1. 프론트엔드 포지션이 생긴 이유, 앞으로 어떻게 될지 2.
+                프론트엔드 개발자의 업무 범위 3. 잘하는 프론트엔드 개발자란?
+                혹시 그럼 이런 글도 혹시 쓰실 생각 있으실까요? 제 질문이지만
+                아마 다른 분들도 궁금해할 질문같아서요 ㅎㅎ 1. 프론트엔드
+                포지션이 생긴 이유, 앞으로 어떻게 될지 2. 프론트엔드 개발자의
+                업무 범위 3. 잘하는 프론트엔드 개발자란?
+            </p>
             <div className="tags-wrap">
                 <a href="/tags/lorem">lorem</a>
             </div>
@@ -50,12 +59,12 @@ const MyCardContainer = styled.section`
         line-height: 25px;
         margin: 8px 0 32px 0;
         /* 3줄 말줄임 */
-        display: -webkit-box; 
-        -webkit-box-orient: vertical; 
-        word-wrap: break-word; 
-        text-overflow: ellipsis; 
-        overflow: hidden; 
-        -webkit-line-clamp: 3; 
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        word-wrap: break-word;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        -webkit-line-clamp: 3;
         height: 70px;
     }
 
