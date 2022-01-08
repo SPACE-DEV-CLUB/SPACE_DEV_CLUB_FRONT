226ac0426bf4672bc1225bc36bd33efa28cc66ee
HyunHo Lee <dlgusgh2001@naver.com>
2022-01-08 20:27:23 +0900

    💄댓글 폼 스타일

--- src/components/Details/Comment.tsx
+++ src/components/Details/Comment.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { PALLETS } from "../../constants";
 
 export const Comment = () => {
   const comment = {
@@ -20,17 +21,56 @@ export const Comment = () => {
   return (
     <article>
       <h3 className="sr-only">상세 페이지 댓글</h3>
-      <form action="">
+      <CommentForm action="">
         <CommentNum>{comment.user.length}개의 댓글</CommentNum>
-        <TextArea name="댓글 입력" placeholder="댓글을 작성하세요" />
-        <button type="submit">댓글 작성</button>
-      </form>
+        <TextArea name="댓글 입력" placeholder="댓글을 작성하세요"></TextArea>
+        <BtnContainer>
+          <CommentBtn type="submit">댓글 작성</CommentBtn>
+        </BtnContainer>
+      </CommentForm>
     </article>
   );
 };
 
+const CommentForm = styled.form`
+  width: 100%;
+`;
 const CommentNum = styled.h4`
   font-size: 18px;
   margin: 24px 0;
 `;
-const TextArea = styled.textarea``;
+const TextArea = styled.textarea`
+  resize: none;
+  padding: 20px 20px 30px;
+  outline: none;
+  border: 1px solid rgb(233, 236, 239);
+  margin-bottom: 1.5rem;
+  width: 100%;
+  border-radius: 4px;
+  min-height: 90px;
+  font-size: 16px;
+  color: rgb(33, 37, 41);
+  line-height: 1.75;
+  ::placeholder {
+    color: rgb(173, 181, 189);
+  }
+`;
+const BtnContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  padding-left: 45px;
+`;
+const CommentBtn = styled.button`
+  font-weight: bold;
+  cursor: pointer;
+  background: ${PALLETS.MAIN};
+  color: white;
+  border-radius: 4px;
+  padding: 0px 20px;
+  height: 32px;
+  font-size: 16px;
+  :hover {
+    opacity: 0.9;
+  }
+`;
