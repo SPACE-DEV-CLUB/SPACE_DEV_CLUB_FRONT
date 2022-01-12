b18eb010a08f70e008b8328962a6be42ac06666b
deli-ght <minjju0660@gmail.com>
2022-01-13 01:58:46 +0900

    ✨ upgrade series - 💄  update series edit ui style - ✨  add series sort

--- src/types/Main.ts
+++ src/types/Main.ts
@@ -1,35 +1,32 @@
 export interface Main {
-  name: string;
-  born: string;
-  died: string;
+  name: string
+  born: string
+  died: string
   bio: {
-    text: string;
-    url: string;
-  };
-  id: number;
+    text: string
+    url: string
+  }
+  id: number
 }
 
 export interface CardProps {
-  imageUrl: string;
-  postTitle: string;
-  postDesc: string;
-  tags: string[];
-  date: number;
-  comment: number;
-  postNum?: number;
+  imageUrl: string
+  postTitle: string
+  postDesc: string
+  tags: string[]
+  date: number
+  comment: number
+  postNum?: number
 }
 
-
-
 export interface DetailCardProps {
-  margin?: string;
-  padding?: string;
-  imageUrl?: string;
-  postIdx: number;
-  postTitle: string;
-  postDesc: string;
-  date: string;
-  id?:number|
-  string;
-
+  margin?: string
+  padding?: string
+  opacity?: boolean
+  imageUrl?: string
+  postIdx: number
+  postTitle: string
+  postDesc: string
+  date: string
+  id?: number | string
 }
