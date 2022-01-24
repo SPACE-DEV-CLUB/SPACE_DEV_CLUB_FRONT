cdf5f8e3955f1342f0c4aef5bd1cb594c9e02b4b
HyunHo Lee <dlgusgh2001@naver.com>
2022-01-24 11:25:41 +0900

    ✨ 동적 라우팅 및 strapi 데이터 연결 🐛 콘솔 에러 해결 ✨동적 라우팅에 따른 상세 페이지 노출(strapi) ✨글 제목, 글 내용, 유저 닉네임 상세 페이지에 표시 ♻️ isEmptyObj함수 제거 🐛 vercel 버그 해결!!

--- src/components/Details/Carousel.tsx
+++ src/components/Details/Carousel.tsx
@@ -1,52 +1,48 @@
-import { useState } from "react"
-import styled from "@emotion/styled"
-import { css } from "@emotion/react"
-import Link from "next/link"
+import { useState } from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import Link from "next/link";
 
-import ArrowBackIcon from "@mui/icons-material/ArrowBack"
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward"
-import { PALLETS_LIGHT } from "../../constants/index"
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { PALLETS_LIGHT } from "../../constants/index";
 
 type IndexTypeProps = {
-  index: number
-}
+  index: number;
+};
 
 export const Carousel = () => {
   const detailData = {
     id: 0,
     content: [
       {
-        src: "https://cdn.pixabay.com/photo/2021/12/12/18/04/mountains-6865752_960_720.jpg",
         href: "1",
         title: "시리즈1",
       },
       {
-        src: "https://cdn.pixabay.com/photo/2021/11/09/15/32/christmas-6781762_960_720.jpg",
         href: "2",
         title: "시리즈2",
       },
       {
-        src: "https://cdn.pixabay.com/photo/2021/11/09/15/32/christmas-6781762_960_720.jpg",
         href: "2",
         title: "시리즈3",
       },
       {
-        src: "https://cdn.pixabay.com/photo/2021/12/21/14/47/castle-6885449_960_720.jpg",
         href: "3",
         title: "시리즈4",
       },
     ],
-  }
-  const [caroucelIndex, setCaroucelIndex] = useState(0)
+  };
+  const [caroucelIndex, setCaroucelIndex] = useState(0);
 
   const handlePrevBtn = () => {
-    if (caroucelIndex === 0) return
-    setCaroucelIndex(caroucelIndex - 1)
-  }
+    if (caroucelIndex === 0) return;
+    setCaroucelIndex(caroucelIndex - 1);
+  };
   const handleNextBtn = () => {
-    if (caroucelIndex === detailData.content.length - 1) return
-    setCaroucelIndex(caroucelIndex + 1)
-  }
+    if (caroucelIndex === detailData.content.length - 1) return;
+    setCaroucelIndex(caroucelIndex + 1);
+  };
 
   return (
     <Container>
@@ -63,12 +59,12 @@ export const Carousel = () => {
         <CarouselItem index={caroucelIndex}>
           {detailData.content.map((s, i) => {
             return (
-              <Link href={s.href} key={`carousel-${s}`} passHref>
+              <Link href={s.href} key={`carousel-${i}`} passHref>
                 <CarouselLink>
                   <CarouselTitle>{s.title}</CarouselTitle>
                 </CarouselLink>
               </Link>
-            )
+            );
           })}
         </CarouselItem>
       </CarouselContainer>
@@ -82,37 +78,33 @@ export const Carousel = () => {
         )}
       </NextContainer>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.section`
   display: flex;
   align-items: center;
   justify-content: space-around;
   margin: 60px 0;
-`
+`;
 const CarouselContainer = styled.article`
   width: 370px;
   height: 50px;
   overflow: hidden;
-`
+`;
 const caroucelIndex = (props: IndexTypeProps) => css`
   transform: translate3d(-${370 * props.index}px, 0, 0);
-`
+`;
 const CarouselItem = styled.div`
   display: flex;
   ${caroucelIndex}
   transition: all .5s ease-in-out;
-`
-// const Img = styled.img`
-//   width: 500px;
-//   height: 500px;
-// `;
-const PrevContainer = styled.div``
+`;
+const PrevContainer = styled.div``;
 const NextContainer = styled.div`
   display: flex;
   justify-content: end;
-`
+`;
 const Btn = styled.button`
   width: 40px;
   height: 40px;
@@ -125,13 +117,13 @@ const Btn = styled.button`
     color: #fff;
     border: 1px solid ${PALLETS_LIGHT.MAIN};
   }
-`
+`;
 const CarouselLink = styled.a`
   width: 370px;
   height: 50px;
   background-color: ${PALLETS_LIGHT.SUB};
   border-radius: 15px;
-`
+`;
 const CarouselTitle = styled.p`
   display: flex;
   justify-content: center;
@@ -141,4 +133,4 @@ const CarouselTitle = styled.p`
   width: 370px;
   height: 50px;
   color: ${PALLETS_LIGHT.MAIN};
-`
+`;
