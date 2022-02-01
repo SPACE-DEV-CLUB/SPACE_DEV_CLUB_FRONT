35268cdd3e5c2debdb7c77aedfb6d876bbbaf7bf
gyuri <jae04099@catholic.ac.kr>
2022-02-01 17:48:52 +0900

    :lipstick: update skeleton loading, :art: block search empty space

--- src/components/Common/SkeletonLoading.tsx
+++ src/components/Common/SkeletonLoading.tsx
@@ -1,25 +1,29 @@
 import { keyframes } from "@emotion/react";
 import styled from "@emotion/styled";
 import React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../../pages/_app";
+import { ThemeProps } from "../../types/Theme";
 
 export default function SkeletonLoading() {
-    const TOP:number[] = [235, 78, 195, 79, 167];
-    const MID_TOP:number[] = [80, 120];
-    const MID_BOTTOM:number[] = [64, 96, 80];
-    const LAST:number[] = [
+    const { theme } = useContext(ThemeContext);
+    const TOP: number[] = [235, 78, 195, 79, 167];
+    const MID_TOP: number[] = [80, 120];
+    const MID_BOTTOM: number[] = [64, 96, 80];
+    const LAST: number[] = [
         68, 112, 45, 158, 136, 112, 90, 103, 51, 155, 77, 180, 103, 51, 115, 58,
-        115, 86, 58, 202, 86, 135, 203, 101, 68, 101
+        115, 86, 58, 202, 86, 135, 203, 101, 68, 101,
     ];
 
     return (
         <>
-            <TopCont>
+            <TopCont theme={theme}>
                 {TOP.map((item, index) => (
                     <li key={`${item}_${index}`} style={{ width: item }}></li>
                 ))}
             </TopCont>
-            <MidCont>
-                <MidTopCont>
+            <MidCont theme={theme}>
+                <MidTopCont theme={theme}>
                     {MID_TOP.map((item, index) => (
                         <li
                             key={`${item}_${index}`}
@@ -27,7 +31,7 @@ export default function SkeletonLoading() {
                         ></li>
                     ))}
                 </MidTopCont>
-                <MidBottomCont>
+                <MidBottomCont theme={theme}>
                     {MID_BOTTOM.map((item, index) => (
                         <li
                             key={`${item}_${index}`}
@@ -36,9 +40,9 @@ export default function SkeletonLoading() {
                     ))}
                 </MidBottomCont>
             </MidCont>
-            <BottomCont></BottomCont>
-            <LastCont>
-                <LastComp>
+            <BottomCont theme={theme}></BottomCont>
+            <LastCont theme={theme}>
+                <LastComp theme={theme}>
                     <ul>
                         {LAST.map((item, index) => (
                             <li
@@ -48,7 +52,7 @@ export default function SkeletonLoading() {
                         ))}
                     </ul>
                 </LastComp>
-                <LastComp>
+                <LastComp theme={theme}>
                     <ul>
                         {LAST.map((item, index) => (
                             <li
@@ -58,7 +62,7 @@ export default function SkeletonLoading() {
                         ))}
                     </ul>
                 </LastComp>
-                <LastComp>
+                <LastComp theme={theme}>
                     <ul>
                         {LAST.map((item, index) => (
                             <li
@@ -74,49 +78,53 @@ export default function SkeletonLoading() {
 }
 
 const fade = keyframes`
-    0%{
-        background: #f3f4f6;
-    }
-    50% {
-        background: #f8f8f8;
-    }
-    100% {
-        background: #f3f4f6;
-    }
+  from{
+    opacity : 100%;
+  }
+  50%{
+    opacity : 30%
+  }
+  to{
+    opacity : 100%;
+  }
 `;
 
-const TopCont = styled.ul`
+const TopCont = styled.ul<ThemeProps>`
     display: flex;
     margin-bottom: 32px;
+    margin-top: 32px;
     li {
         width: 100%;
         border-radius: 5px;
         height: 60px;
         margin-right: 8px;
+        background: ${({ theme }) => theme.TOGGLE_BACKGROUND};
         animation: ${fade} 2s infinite;
     }
 `;
-const MidCont = styled.div`
+const MidCont = styled.div<ThemeProps>`
     display: flex;
     flex-direction: column;
     li {
         height: 16px;
         border-radius: 5px;
         margin-right: 24px;
+        background: ${({ theme }) => theme.TOGGLE_BACKGROUND};
         animation: ${fade} 2s infinite;
     }
 `;
-const MidTopCont = styled.ul`
+const MidTopCont = styled.ul<ThemeProps>`
     display: flex;
     margin-bottom: 17px;
     li {
         height: 16px;
         border-radius: 3px;
         margin-right: 24px;
+        background: ${({ theme }) => theme.TOGGLE_BACKGROUND};
         animation: ${fade} 2s infinite;
     }
 `;
-const MidBottomCont = styled.ul`
+const MidBottomCont = styled.ul<ThemeProps>`
     display: flex;
     margin-bottom: 39px;
 
@@ -125,17 +133,19 @@ const MidBottomCont = styled.ul`
         border-radius: 5px;
         margin-right: 8px;
         animation: ${fade} 2s infinite;
+        background: ${({ theme }) => theme.TOGGLE_BACKGROUND};
     }
 `;
-const BottomCont = styled.div`
+const BottomCont = styled.div<ThemeProps>`
     width: 100%;
     height: 401px;
     border-radius: 5px;
     animation: ${fade} 2s infinite;
+    background: ${({ theme }) => theme.TOGGLE_BACKGROUND};
     margin-bottom: 80px;
 `;
 const LastCont = styled.div``;
-const LastComp = styled.div`
+const LastComp = styled.div<ThemeProps>`
     display: flex;
     ul {
         display: flex;
@@ -148,7 +158,6 @@ const LastComp = styled.div`
         margin-right: 8px;
         margin-bottom: 12px;
         animation: ${fade} 2s infinite;
+        background: ${({ theme }) => theme.TOGGLE_BACKGROUND};
     }
 `;
-//F3F4F6
-//F8F8F8
