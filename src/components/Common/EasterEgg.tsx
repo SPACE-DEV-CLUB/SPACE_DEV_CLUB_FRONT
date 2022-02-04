080dee78f6dd25fe6f9b5156e9cdf8138725bbd7
gyurilee <jae04099@gmail.com>
2022-02-04 17:24:06 +0900

    :sparkles: add intropage link

---
+++ src/components/Common/EasterEgg.tsx
@@ -0,0 +1,46 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { Egg } from "@mui/icons-material";
+import Link from "next/link";
+import React from "react";
+
+function EasterEgg() {
+    return (
+        <>
+            <LinkWrap>
+                <Link href="/introduce">
+                    <EggIcon />
+                </Link>
+            </LinkWrap>
+        </>
+    );
+}
+
+export default EasterEgg;
+
+const Shake = keyframes`
+    0% {
+        transform: rotateZ(0deg);
+    }
+    25% {
+        transform: rotateZ(-50deg);
+    }
+    75% {
+        transform: rotateZ(50deg);
+    }
+    100% {
+        transform: rotateZ(0deg);
+    }
+`
+const LinkWrap = styled.div`
+    position: fixed;
+    bottom: 30px;
+    left: 30px;
+    cursor: pointer;
+    &:hover {
+        animation: ${Shake} 1s;
+    }
+`
+const EggIcon = styled(Egg)`
+    font-size: 32px;
+`
