53b8a7248801bf67a8675893f499cf41fa830c10
kykim00 <jeongsaeki@likelion.org>
2022-02-04 20:44:15 +0900

    ♻️ 날짜 형식 몇 시간전 추가 🐛 날짜 형식 버그 수정 (ex. 1월 51일)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,13 @@
 let today = new Date();
 let todayDay = today.getDay();
 
+const handleInADay = (day: Date) => {
+  day.setDate(day.getDate() - 7);
+  const dateDiff = (today.getTime() - day.getTime()) / (1000 * 3600);
+  if (dateDiff < 1) return '방금 전';
+  else return Math.floor(dateDiff) + '시간 전';
+};
+
 export const handleDate = (createdAt: string) => {
   let day = new Date(createdAt);
   day.setDate(day.getDate() + 7);
@@ -8,16 +15,15 @@ export const handleDate = (createdAt: string) => {
     day.setDate(day.getDate() - 7);
     return (
       day.getFullYear() +
-      "년 " +
+      '년 ' +
       (day.getMonth() + 1) +
-      "월 " +
-      day.getDay() +
-      1 +
-      "일"
+      '월 ' +
+      day.getDate() +
+      '일'
     );
   } else {
     return day.getDay() - todayDay === 0
-      ? "오늘"
-      : Math.abs(day.getDay() - todayDay) + "일전";
+      ? handleInADay(day)
+      : Math.abs(day.getDay() - todayDay) + '일전';
   }
 };
