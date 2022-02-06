1574519f6b1b524d21af983b5b2fa81bf5a8acad
HyunHo Lee <dlgusgh2001@naver.com>
2022-02-06 20:26:38 +0900

    🔨 'OO 분 전' 구현

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,29 +1,34 @@
 let today = new Date();
 let todayDay = today.getDay();
 
+const handleTime = (day: Date) => {
+  const timeDiff = today.getMinutes() - day.getMinutes();
+  return timeDiff < 1 ? "방금전" : Math.floor(timeDiff) + "분 전";
+};
+
 const handleInADay = (day: Date) => {
   day.setDate(day.getDate() - 7);
   const dateDiff = (today.getTime() - day.getTime()) / (1000 * 3600);
-  if (dateDiff < 1) return '방금 전';
-  else return Math.floor(dateDiff) + '시간 전';
+  return dateDiff < 1 ? handleTime(day) : Math.floor(dateDiff) + "시간 전";
 };
 
 export const handleDate = (createdAt: string) => {
   let day = new Date(createdAt);
   day.setDate(day.getDate() + 7);
-  if (today.getTime() > day.getTime()) {
+  const diff = today.getTime() - day.getTime();
+  if (diff > 0) {
     day.setDate(day.getDate() - 7);
     return (
       day.getFullYear() +
-      '년 ' +
+      "년 " +
       (day.getMonth() + 1) +
-      '월 ' +
+      "월 " +
       day.getDate() +
-      '일'
+      "일"
     );
   } else {
     return day.getDay() - todayDay === 0
       ? handleInADay(day)
-      : Math.abs(day.getDay() - todayDay) + '일전';
+      : Math.abs(day.getDay() - todayDay) + "일전";
   }
 };
