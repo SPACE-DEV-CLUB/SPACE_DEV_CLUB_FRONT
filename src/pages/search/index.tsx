acd9ba83f6be79725dde85ca26e8bff6e3efe995
gyurilee <jae04099@gmail.com>
2022-02-26 17:51:46 +0900

    :sparkles: add dynamic username

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -4,29 +4,34 @@ import { API_ENDPOINT, MEDIA_QUERY_END_POINT } from "../../constants";
 import { Header } from "@components/Common/Header";
 import SearchHead from "@components/Search/SearchHead";
 import SearchFilter from "@components/Search/SearchFilter";
+import { useRouter } from "next/router";
 
 const MyPage: NextPage = () => {
-
-    return (
-        <>
-            <SearchHead />
-            <Header username={"deli-ght"} user={false} />
-            <Container>
-                <SearchFilter />
-            </Container>
-        </>
-    );
+  const router = useRouter();
+  const { username } = router.query;
+  return (
+    <>
+      <SearchHead />
+      <Header
+        username={username ? `${username}` : ""}
+        user={username ? true : false}
+      />
+      <Container>
+        <SearchFilter />
+      </Container>
+    </>
+  );
 };
 
 export default MyPage;
 
 const Container = styled.div`
-    margin: 0 auto;
-    max-width: 734px;
-    @media only screen and (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
-        padding: 20px;
-        input {
-            height: 27px;
-        }
+  margin: 0 auto;
+  max-width: 734px;
+  @media only screen and (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
+    padding: 20px;
+    input {
+      height: 27px;
     }
+  }
 `;
