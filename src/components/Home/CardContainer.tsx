ec0e82983f7e43470811e48987f8c93b6ef560ad
ongddree <ongddree@gmail.com>
2022-03-08 00:09:51 +0900

    💄  카드 컨테이너 반응형 조건 max-width로 변경
    💄  필터 반응형 조건 max-width로 변경

--- src/components/Home/CardContainer.tsx
+++ src/components/Home/CardContainer.tsx
@@ -144,41 +144,37 @@ export const CardContainer = ({ filter }: { filter: string }) => {
 };
 const Container = styled.section`
   display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  gap: 16px;
   margin: 0 auto 16px;
-  @media (min-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
-    grid-template-columns: repeat(2, 1fr);
-    gap: 32px;
-    margin: 0 auto 16px;
-  }
-
-  @media (min-width: ${MEDIA_QUERY_END_POINT.TABLET}) {
-    grid-template-columns: repeat(3, 1fr);
-    gap: 32px;
-    margin-bottom: 32px;
-    max-width: ${MEDIA_QUERY_END_POINT.TABLET};
-  }
-
-  @media (min-width: ${MEDIA_QUERY_END_POINT.DESKTOP}) {
-    grid-template-columns: repeat(3, 1fr);
-    gap: 32px;
-    margin-bottom: 32px;
-    max-width: ${MEDIA_QUERY_END_POINT.TABLET};
-  }
-
-  @media (min-width: ${MEDIA_QUERY_END_POINT.LARGE}) {
+  grid-template-columns: repeat(5, 1fr);
+  gap: 32px;
+  margin-bottom: 32px;
+  max-width: 1728px; 
+  @media screen and (max-width: ${MEDIA_QUERY_END_POINT.XLARGE}) {
     grid-template-columns: repeat(4, 1fr);
     gap: 32px;
     margin-bottom: 32px;
     max-width: 1376px;
   }
-
-  @media (min-width: ${MEDIA_QUERY_END_POINT.XLARGE}) {
-    grid-template-columns: repeat(5, 1fr);
+  @media screen and (max-width: ${MEDIA_QUERY_END_POINT.LARGE}) {
+    grid-template-columns: repeat(3, 1fr);
     gap: 32px;
     margin-bottom: 32px;
-    max-width: 1728px;
+    max-width: ${MEDIA_QUERY_END_POINT.TABLET};
+  }
+  @media screen and (max-width: ${MEDIA_QUERY_END_POINT.DESKTOP}) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 32px;
+    margin-bottom: 32px;
+    max-width: ${MEDIA_QUERY_END_POINT.TABLET};
+  }
+  @media screen and (max-width: ${MEDIA_QUERY_END_POINT.TABLET}) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 32px;
+    margin: 0 auto 16px;
+  }
+  @media screen and (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
+    grid-template-columns: repeat(1, 1fr);
+    gap: 16px;
   }
 `;
 
