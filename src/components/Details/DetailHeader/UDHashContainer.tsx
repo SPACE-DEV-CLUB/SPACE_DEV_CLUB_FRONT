9c9abf30f3adb333d6c9a676768a001ad9dc6fef
HyunHo Lee <dlgusgh2001@naver.com>
2022-03-13 12:43:36 +0900

    ✨게시글 삭제 및 게시글에 달린 댓글과 대댓글 삭제

--- src/components/Details/DetailHeader/UDHashContainer.tsx
+++ src/components/Details/DetailHeader/UDHashContainer.tsx
@@ -2,14 +2,18 @@ import styled from "@emotion/styled";
 import Link from "next/link";
 
 import { Theme } from "@styles/theme";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "@pages/_app";
 
 import { handleDate } from "@utils/date";
+import { Modal } from "@src/components/Common/Modal";
 
 import { Tag } from "../../Common/Tag";
 import { PostContext } from "@pages/[id]/[details]";
 import { useRouter } from "next/router";
+import axios, { Method } from "axios";
+import { API_ENDPOINT } from "@src/constants";
+import { CommentData } from "@src/types/Detail";
 
 interface ThemeProps {
   theme: Theme;
@@ -22,8 +26,39 @@ interface Props {
 
 export const UDHashContainer = ({ userName, loginUserId }: Props) => {
   const { theme } = useContext(ThemeContext);
-  const { postObj } = useContext(PostContext);
+  const { postObj, postid } = useContext(PostContext);
+  const [isDelete, setIsDelete] = useState(false);
   const router = useRouter();
+
+  const onClickDelete = () => {
+    setIsDelete(true);
+  };
+
+  const onClickNo = () => {
+    setIsDelete(false);
+    document.body.style.overflow = "unset";
+  };
+
+  const Delete = async (id: number) => {
+    await axios({
+      method: "delete" as Method,
+      url: `${API_ENDPOINT}/comments/${id}`,
+    });
+  };
+
+  const onClickYes = async () => {
+    const res = await axios({
+      method: "get" as Method,
+      url: `${API_ENDPOINT}/comments?populate=*&filters[posts][id]=${postid}`,
+    });
+    res.data.data.forEach((data: CommentData) => Delete(data.id));
+    await axios({
+      method: "delete" as Method,
+      url: `${API_ENDPOINT}/posts/${postid}`,
+    });
+    router.push("/");
+    document.body.style.overflow = "unset";
+  };
 
   const onClickNickname = () => {
     router.push(`/${userName}`);
@@ -47,9 +82,9 @@ export const UDHashContainer = ({ userName, loginUserId }: Props) => {
             <Link href="#" passHref>
               <UDItem theme={theme}>수정</UDItem>
             </Link>
-            <Link href="#" passHref>
-              <UDItem theme={theme}>삭제</UDItem>
-            </Link>
+            <UDItem onClick={onClickDelete} type="button" theme={theme}>
+              삭제
+            </UDItem>
           </div>
         )}
       </UDContainer>
@@ -58,6 +93,16 @@ export const UDHashContainer = ({ userName, loginUserId }: Props) => {
           <Tag key={`Detail_tag_${tag.id}`} tagName={tag.attributes.name}></Tag>
         );
       })}
+      {isDelete && (
+        <Modal
+          title="포스트 삭제"
+          handleOK={onClickYes}
+          check={isDelete}
+          handleCancel={onClickNo}
+        >
+          정말로 삭제하시겠습니까?
+        </Modal>
+      )}
     </article>
   );
 };
@@ -84,7 +129,7 @@ const CreatedAt = styled.span<ThemeProps>`
     color: ${({ theme }) => theme.SUB_FONT};
   }
 `;
-const UDItem = styled.a<ThemeProps>`
+const UDItem = styled.button<ThemeProps>`
   color: ${({ theme }) => theme.ICON};
   font-weight: 500;
   margin-right: 7px;
